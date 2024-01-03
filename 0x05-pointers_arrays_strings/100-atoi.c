556531626b20cc14141ccbb5b106a3e8fcf50d98
Dada Musa <olamilekandada23@gmail.com>
2024-01-03 14:14:17 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,43 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 #include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
- * @s: The string to convert.
+ * @s: The input string.
  *
  * Return: The converted integer.
  */
 int _atoi(char *s)
-
 {
-	int sign = 1;
 	int result = 0;
-	int i = 0;
+	int sign = 1;
+	int digit;
 
-	while (s[i] != '\0' && (s[i] == '-' || s[i] == '+'))
+	while (*s == ' ')
+		s++;
+
+	if (*s == '-' || *s == '+')
 	{
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-		i++;
+		sign = (*s == '-') ? -1 : 1;
+		s++;
 	}
 
-	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+	while (*s >= '0' && *s <= '9')
 	{
-		int digit = s[i] - '0';
-
+		digit = *s - '0';
 		if (result > (INT_MAX - digit) / 10)
 		{
-			if (sign == 1)
-				return (INT_MAX);
-			else
-				return (INT_MIN);
+			return ((sign == 1) ? INT_MAX : INT_MIN);
 		}
 		result = result * 10 + digit;
-		i++;
+		s++;
 	}
-	return (result * sign);
+
+	return (sign * result);
 }
-
-
