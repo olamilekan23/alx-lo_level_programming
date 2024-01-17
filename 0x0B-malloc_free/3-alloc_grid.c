8f43f06f0ea60c0836433f196f6d0e07790ba1f4
Dada Musa Olamilekan <olamilekandada23@gmail.com>
2024-01-17 11:02:31 +0100

    4-free_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int **output;
 
 	if (width <= 0 || height <= 0)
-		return(NULL);
+		return (NULL);
 
 	output = malloc(sizeof(int *) * height);
 
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a 2 dimensional grid created by alloc_grid
+ * @grid: the address of the two-dimensional grid
+ * @height: height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
