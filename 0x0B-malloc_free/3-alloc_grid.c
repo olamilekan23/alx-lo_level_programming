595506bbe1560cdee3d001c4522e60b9b9e16faa
Dada Musa Olamilekan <olamilekandada23@gmail.com>
2024-01-17 10:51:29 +0100

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 				free(output[j]);
 			return (NULL);
 		}
-		for (j = 0; j < width; j)
+		for (j = 0; j < width; j++)
 			output[i][j] = 0;
 	}
 	return (output);
